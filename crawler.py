7010f3048cfe439ca94f3cecbda16335d6ab300d
johnrau <luc.thang@gmail.com>
2023-10-11 19:51:58 +0700

    fix issue wrong class name

--- crawler.py
+++ crawler.py
@@ -10,12 +10,9 @@ content = result.text
 soup = BeautifulSoup(content, 'lxml')
 # print(soup.prettify())
 
-box = soup.find('article', class_='main_article')
-print(" ============ ")
-print(box)
-print(" ============ ")
+box = soup.find('article', class_='main-article')
 title = box.find('h1').get_text()
-# print(title)
+
 transcript = box.find('div',class_='full-script').get_text(strip=True, separator=' ')
 # print(transcript)
 with open(f'{title}.txt', 'w', encoding='UTF-8') as file: 

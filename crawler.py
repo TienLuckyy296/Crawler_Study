260e691c87c8fce39f40135a7be2f37495514fb7
TienLuckyy296 <tiennaruto123@gmail.com>
2023-10-14 20:16:47 +0700

    first

--- crawler.py
+++ crawler.py
@@ -1,19 +1,31 @@
 from bs4 import BeautifulSoup 
 import requests 
-# import sys
-# import codecs
-# sys.stdout = codecs.getwriter("utf-8")(sys.stdout, 'xmlcharrefreplace')
-website = 'https://subslikescript.com/movie/Titanic-120338'
+
+root = 'https://subslikescript.com'
+website = f'{root}/movies'
 result = requests.get(website)
 content = result.text 
-
 soup = BeautifulSoup(content, 'lxml')
 # print(soup.prettify())
 
 box = soup.find('article', class_='main-article')
-title = box.find('h1').get_text()
 
-transcript = box.find('div',class_='full-script').get_text(strip=True, separator=' ')
-# print(transcript)
-with open(f'{title}.txt', 'w', encoding='UTF-8') as file: 
-    file.write(transcript)
+links = []
+for link in box.find_all('a', href=True):
+    links.append(link['href'])
+
+print(links)
+
+for link in links: 
+    website = f'{root}/{link}'
+    result = requests.get(website)
+    content = result.text 
+    soup = BeautifulSoup(content, 'lxml')
+
+    box = soup.find('article', class_='main-article')
+
+    title = box.find('h1').get_text()
+    transcript = box.find('div',class_='full-script').get_text(strip=True, separator=' ')
+
+    with open(f'{title}.txt', 'w', encoding='UTF-8') as file: 
+        file.write(transcript)

1aac699d7ef79a43061fa45a09f05278b1695706
TienLuckyy296 <tiennaruto123@gmail.com>
2023-10-15 22:11:24 +0700

    15-10

--- crawler.py
+++ crawler.py
@@ -2,30 +2,45 @@ from bs4 import BeautifulSoup
 import requests 
 
 root = 'https://subslikescript.com'
-website = f'{root}/movies'
+website = f'{root}/movies_letter-A'
 result = requests.get(website)
 content = result.text 
 soup = BeautifulSoup(content, 'lxml')
 # print(soup.prettify())
 
-box = soup.find('article', class_='main-article')
+pagination = soup.find('ul',class_='pagination')
+pages = pagination.find_all('li',class_='page-item')
+last_page = pages[-2].text
 
 links = []
-for link in box.find_all('a', href=True):
-    links.append(link['href'])
 
-print(links)
-
-for link in links: 
-    website = f'{root}/{link}'
-    result = requests.get(website)
+for page in range (1, int(last_page)+1): # [1,92+1] range 
+    # https: // subslikescript.com / movies_letter - A?page = 1
+    result = requests.get(f'{website}?page={page}')
     content = result.text 
     soup = BeautifulSoup(content, 'lxml')
 
     box = soup.find('article', class_='main-article')
 
-    title = box.find('h1').get_text()
-    transcript = box.find('div',class_='full-script').get_text(strip=True, separator=' ')
+    links = []
+    for link in box.find_all('a', href=True):
+        links.append(link['href'])
 
-    with open(f'{title}.txt', 'w', encoding='UTF-8') as file: 
-        file.write(transcript)
+    for link in links: 
+        try: 
+            print(link)
+            result = requests.get(f'{root}/{link}')
+            content = result.text 
+            soup = BeautifulSoup(content, 'lxml')
+
+            box = soup.find('article', class_='main-article')
+
+            title = box.find('h1').get_text()
+            transcript = box.find('div',class_='full-script').get_text(strip=True, separator=' ')
+        except:  
+            print('------------Link not working---------------')
+            print(link) 
+        
+        
+        with open(f'{title}.txt', 'w', encoding='UTF-8') as file: 
+            file.write(transcript)

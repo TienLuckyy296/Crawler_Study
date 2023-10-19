fb5c50f087f1b352418f89dd833ecb466954dca6
TienLuckyy296 <tiennaruto123@gmail.com>
2023-10-19 08:33:18 +0700

    3 days

--- crawler2.py
+++ crawler2.py
@@ -1,8 +1,11 @@
 from selenium import webdriver
 website = 'https://www.adamchoi.co.uk/overs/detailed'
 # path nay la duong dan den thu muc chromedriver download ve tren may cua minh
-path = 'D:\\Working\\study\\chromedriver'
+path = 'C:\\Users\\TienLN\\Downloads\\chromedriver-win64'
 driver = webdriver.Chrome()
 driver.get(website)
 
-driver.quit()
+all_matches_button = driver.find_element_by_xpath('//label[@analytics-event="All matches"]')
+all_matches_button.click()
+
+#driver.quit()

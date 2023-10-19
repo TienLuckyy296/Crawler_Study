ebe1fdf945c99ba3661c773172eaf88e3ebfb131
TienLuckyy296 <tiennaruto123@gmail.com>
2023-10-19 11:17:23 +0700

    update common by for selenium 4.3

--- crawler2.py
+++ crawler2.py
@@ -1,11 +1,19 @@
+import time
 from selenium import webdriver
+from selenium.webdriver.common.by import By
 website = 'https://www.adamchoi.co.uk/overs/detailed'
 # path nay la duong dan den thu muc chromedriver download ve tren may cua minh
 path = 'C:\\Users\\TienLN\\Downloads\\chromedriver-win64'
 driver = webdriver.Chrome()
 driver.get(website)
-
-all_matches_button = driver.find_element_by_xpath('//label[@analytics-event="All matches"]')
+time.sleep(3000)
+all_matches_button = driver.find_element("xpath",'//label[@analytics-event="All matches"]')
 all_matches_button.click()
 
+matches = driver.find_element(By.TAG_NAME, "tr")
+print(matches)
+for match in matches :
+    print(match.text)
+
+time.sleep(3000)
 #driver.quit()

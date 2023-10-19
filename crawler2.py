8c58e6d6985e23ecb33414fc3cfc332e77d4c323
johnrau <luc.thang@gmail.com>
2023-10-19 13:21:24 +0700

    use drivermanager package

--- crawler2.py
+++ crawler2.py
@@ -1,16 +1,20 @@
 import time
 from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.service import Service as ChromeService
 from selenium.webdriver.common.by import By
 website = 'https://www.adamchoi.co.uk/overs/detailed'
-# path nay la duong dan den thu muc chromedriver download ve tren may cua minh
+
+# path = 'D:\\Working\\study\\chromedriver'
 path = 'C:\\Users\\TienLN\\Downloads\\chromedriver-win64'
-driver = webdriver.Chrome()
+driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
+# driver = webdriver.Chrome()
 driver.get(website)
-time.sleep(3000)
+
 all_matches_button = driver.find_element("xpath",'//label[@analytics-event="All matches"]')
 all_matches_button.click()
 
-matches = driver.find_element(By.TAG_NAME, "tr")
+matches = driver.find_elements(By.TAG_NAME, "tr")
 print(matches)
 for match in matches :
     print(match.text)

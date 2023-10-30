79afe62ef9cfa912e9787e699bffe07467db31d3
johnrau <luc.thang@gmail.com>
2023-10-30 22:35:24 +0700

    done project1: dealing with multi pages using selenium

--- README.md
+++ README.md
@@ -10,3 +10,13 @@ Beautiful Soup
 Selennium
     pip install selenium
     pip install webdriver-manager (follow guideline for new versions selenium: https://pypi.org/project/webdriver-manager/)
+
+Scrapy:
+    Download and install Anaconda: https://www.anaconda.com/download
+    Create a new environment on Anaconda Navigator: my_scrapy_venv
+    open terminal of my_scrapy_venv and run command to install: conda install -c conda-forge scrapy
+    config python interpreter on VSCode use anaconda environment, example path on Windows: ..\\anaconda3\envs\my_scrapy_venv\python.exe (to check use command: where python or which python)
+    run command setup protego: conda install -c conda-forge protego
+
+Git commands to manage repos:
+https://docs.github.com/en/get-started/getting-started-with-git/managing-remote-repositories
--- multiPages.py
+++ multiPages.py
@@ -4,27 +4,50 @@ from selenium.webdriver.support.ui import Select
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.service import Service as ChromeService
 from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
+import time
+
+options = Options()
+options.headless = True
+options.add_argument('window-size=1920x1080')
 
 web = 'https://www.audible.com/search'
 # path = 'D:\\Working\\study\\chromedriver'
-driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
+driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()),options=options)
 driver.get(web)
-driver.maximize_window()
+# driver.maximize_window()
 
-container = driver.find_element(By.CLASS_NAME, 'adbl-impression-container')
+# pagination: section 33
+pagination = driver.find_element(By.XPATH, '//ul[contains(@class, "pagingElements")]')
+pages = pagination.find_elements(By.TAG_NAME, 'li')
+lastpage = int(pages[-2].text)
 
-products = container.find_elements(By.XPATH, './/li[contains(@class, "productListItem")]')
+current_page = 1
 book_title = []
 book_author = []
 book_length = []
 
-print('products == ;; == ' , products)
-for product in products:
-    book_title.append(product.find_element(By.XPATH, './/h3[contains(@class,"bc-heading")]').text)
-    book_author.append(product.find_element(By.XPATH, './/li[contains(@class,"authorLabel")]').text)
-    book_length.append(product.find_element(By.XPATH, './/li[contains(@class,"runtimeLabel")]').text)
+while current_page <= lastpage:
+    time.sleep(2)
+    container = driver.find_element(By.CLASS_NAME, 'adbl-impression-container')
+
+    products = container.find_elements(By.XPATH, './/li[contains(@class, "productListItem")]')
+
+    for product in products:
+        title = product.find_element(By.XPATH, './/h3[contains(@class,"bc-heading")]').text
+        book_title.append(title)
+        print(title)
+        book_author.append(product.find_element(By.XPATH, './/li[contains(@class,"authorLabel")]').text)
+        book_length.append(product.find_element(By.XPATH, './/li[contains(@class,"runtimeLabel")]').text)
+    
+    current_page = current_page + 1
+    try:
+        next_page = driver.find_element(By.XPATH, '//span[contains(@class, "nextButton")]')
+        next_page.click()
+    except:
+        pass
     
 driver.quit()
 
 df_books = pd.DataFrame({'title': book_title, 'author': book_author, 'length': book_length})
-df_books.to_csv('books.csv', index=False)
+df_books.to_csv('books_pagination_section33.csv', index=False)
